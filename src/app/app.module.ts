daac7c388fb6becaf3365044bef321f8d07e7ee7
Daisuke Sone <sonedai0213@gmail.com>
2020-08-23 20:21:00 +0900

    change link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { WeatherCardComponent } from './weather-card/weather-card.component';
-import { AddCardComponent } from './add-card/add-card.component';
-import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './pages/home/home.component';
+import { WeatherCardComponent } from './ui/weather-card/weather-card.component';
+import { AddCardComponent } from './ui/add-card/add-card.component';
+import { DetailsComponent } from './pages/details/details.component';
 import { AddComponent } from './pages/add/add.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
